bae01fc14fce30da64c628c212655cfa87e9fa9e
Prathamesh <prathameshhh.2004@gmail.com>
2024-09-27 15:26:29 +0530

    Sign up functionality added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+
 
 export default function SignIn() {
   return (
-    <div>
+    <>
       <Header />
-      SignIn
-    </div>
-  )
+      Signin
+    </>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { connectDB } = require("./utils/db");
 const { authRouter } = require("./routes/authRoutes");
 const { globalError } = require("./middlewares/errorHandler");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use(globalError);
