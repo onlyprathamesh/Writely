fdcdb24fd75ae59330d25cddc7a085bb3a4665bc
Prathamesh <prathameshhh.2004@gmail.com>
2024-10-03 18:17:26 +0530

    added searchbar functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,46 @@
 "use client";
-import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { AiOutlineSearch } from "react-icons/ai";
 export default function Header() {
   const location = useLocation();
   const dispatch = useDispatch();
   const isActive = (path) => location.pathname === path;
   const { currentUser } = useSelector((state) => state.user);
-  const {theme} = useSelector((state) => state.theme)
+  const { theme } = useSelector((state) => state.theme);
   return (
-    <Navbar fluid rounded className="border drop-shadow-lg">
+    <Navbar fluid rounded className="border-b-2 drop-shadow-lg">
       <Navbar.Brand href="/">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Writely
         </span>
       </Navbar.Brand>
+      <div>
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+          />
+          <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+            <AiOutlineSearch />
+          </Button>
+        </div>
       <div className="flex md:order-2 gap-2">
-        <Button pill className="w-12 h-10 hidden sm:inline" color={"gray"} onClick={()=> dispatch(toggleTheme())}>
-          {theme === 'dark' ? <FaSun /> : <FaMoon />}
+        <Button
+          pill
+          className="w-12 h-10 hidden sm:inline"
+          color={"gray"}
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === "dark" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
-          <Dropdown className="max-w-30"
+          <Dropdown
+            className="max-w-30"
             arrowIcon={false}
             inline
             label={
